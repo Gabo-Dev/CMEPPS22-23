b773a353944e801b172797f879f98c0d23168bce
jhony <jhony@DESKTOP-VCR4V9E>
2022-10-13 21:37:27 +0200

    Saluda Usuario

--- src/tutorial/HolaWorld.java
+++ src/tutorial/HolaWorld.java
@@ -5,6 +5,8 @@ public class HolaWorld {
 	public static void main(String[] args) {
 		System.out.println("Hola CMEPPS!");
 		System.out.println("Welcome to UHU");
+		String name="Gabo";
+		System.out.println("Hola, "+name);
 	}
 
 }
